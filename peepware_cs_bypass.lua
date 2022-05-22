88a3542d12aeb793298e2f928dde557125c3315f
ao <xibas.fork@gmail.com>
2022-05-22 00:32:37 -0400

    Create peepware_cs_bypass.lua

---
+++ peepware_cs_bypass.lua
@@ -0,0 +1,3 @@
+-- makes it seem bypassed ( so hacker)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NexIsHot/PeepWare/main/Login.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NexIsHot/PeepWare/main/CS.lua"))()
