07ab72c712c1696662466849a8ae203112cd12bd
ao <xibas.fork@gmail.com>
2022-05-22 00:29:50 -0400

    Update cb.lua

--- backups/somahook/cb.lua
+++ backups/somahook/cb.lua
@@ -1,3 +1,13 @@
+local callback = Instance.new("BindableFunction")
+function callback.OnInvoke(a)if a=="Copy" then setclipboard('https://discord.gg/8ps4575qtH') end end
+game:GetService("StarterGui"):SetCore("SendNotification", {
+	Title = "DJ";
+	Text = "CRACKED BY THE DJS, JOIN THE DISCORD FOR MORE";
+	Duration = 5;
+	Button1 = "Copy";
+	Button2 = "No";
+	Callback = callback
+})
 
 makefolder("djhook") 
 wait(0.1)
